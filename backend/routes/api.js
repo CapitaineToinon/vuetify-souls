c327da1adb26559b6ca4b6292ab5eb3a3d347ef9
CapitaineToinon <antoinesutter@gmail.com>
2018-10-09 23:55:40 +0200

    Backend update + eslint
    - Modified the backend to use generators
    - Replace request / cached-request by axios / cachios
    - Proper error handler
    - Removed packages
    - Added eslint memes

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,60 +1,53 @@
-/**
- * Requires
- */
-var express = require('express')
-var router = express.Router()
-var api = require('../bin/api')
-var Handler = require('../bin/errors')
+const express = require('express');
+
+const router = express.Router();
+const api = require('../bin/api');
 
 /**
  * const
  */
-const BASE_URI = 'http://localhost:3000'
-const HandleError = function(error, res) {
-    res.status(Handler.STATUS_CODE).json(new Handler.Error(error.message))
-}
+const BASE_URI = 'http://localhost:3000';
 
-router.get('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*")
-    next()
-})
+router.get('*', (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
 
-router.get('/', function (req, res, next) {
+router.get('/', (req, res) => {
     res.json({
-        "links": {
-            "rel": "self",
-            "uri": BASE_URI + "/api"
-        }
-    })
-})
+        links: {
+            rel: 'self',
+            uri: `${BASE_URI}/api`,
+        },
+    });
+});
 
-router.get('/home', function (req, res, next) {
-    api.getHome((error, game) => {
-        if (error) return HandleError(error, res)
-        res.json(game)
-    })
-})
+router.get('/games', (req, res, next) => {
+    api.getSoulsGames()
+        .then(games => res.json(games))
+        .catch(err => next(err));
+});
 
-router.get('/games', function (req, res, next) {
-    api.getSoulsGames((error, games) => {
-        if (error) return HandleError(error, res)
-        res.json(games)
-    })
-})
+router.get('/runs/:id', (req, res, next) => {
+    api.getRun(req.params.id)
+        .then(run => res.json(run))
+        .catch(err => next(err));
+});
 
-router.get('/leaderboard/:game/:category', function(req, res, next) {
+router.get('/leaderboard/:game/:category', (req, res, next) => {
+    const subCategories = Object.entries(req.query)
+        .filter((item, key) => Object.keys(req.query)[key].startsWith('var-'))
+        .map(([key, value]) => `${key}=${value}`);
 
-    var subCategories = []
-    for(var q in req.query) {
-        if (q.startsWith('var-')) {
-            subCategories.push(q + '=' + req.query[q])
-        }
-    }
+    api.getLeaderboard(req.params.game, req.params.category, subCategories)
+        .then(leaderboard => res.json(leaderboard))
+        .catch(err => next(err));
+});
 
-    api.getLeaderboard(req.params.game, req.params.category, subCategories, (error, leaderboard) => {
-        if (error) return HandleError(error, res)
-        res.json(leaderboard)
-    })
-})
+router.get('*', (req, res, next) => {
+    let error = new Error('');
+    error.code = 404;
+    throw error;
+});
 
 module.exports = router;
